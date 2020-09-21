4aa03af402b5084b6cabb50687f600276b4f5e15
yuizho <yuizho3@gmail.com>
2020-09-21 11:50:24 +0900

    modify sound.rs to turn down the alert volume

--- src/sound.rs
+++ src/sound.rs
@@ -10,14 +10,14 @@ pub struct Player {
 impl Player {
     pub fn new() -> Player {
         Player {
-            device: rodio::default_output_device()
-                .expect("failed to find a sound device"),
+            device: rodio::default_output_device().expect("failed to find a sound device"),
         }
     }
 
     pub fn play(&self, sound_file: impl FileData) -> Result<(), failure::Error> {
         let sink = rodio::play_once(&self.device, Cursor::new(sound_file.get_bytes()))
             .context("filed to play sound")?;
+        sink.set_volume(0.1);
         sink.detach();
         Ok(())
     }
@@ -34,9 +34,7 @@ pub enum SoundFile {
 impl FileData for SoundFile {
     fn get_bytes(&self) -> Vec<u8> {
         match self {
-            SoundFile::BELL => include_bytes!("bell.mp3").to_vec()
+            SoundFile::BELL => include_bytes!("bell.mp3").to_vec(),
         }
     }
 }
-
-
